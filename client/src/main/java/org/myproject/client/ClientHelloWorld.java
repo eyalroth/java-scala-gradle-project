bfa3900f500e81259ac2ee15826581d9379bd234
Eyal Roth <eyalroth1@gmail.com>
2018-12-31 14:27:06 +0200

    Add a second java module (client)

---
+++ client/src/main/java/org/myproject/client/ClientHelloWorld.java
@@ -0,0 +1,9 @@
+package org.myproject.client;
+
+public class ClientHelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ client/src/test/java/org/myproject/client/ClientHelloWorldTest.java
@@ -0,0 +1,14 @@
+package org.myproject.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClientHelloWorldTest {
+
+    @Test
+    public void simpleCalc() {
+
+        assertEquals(0, 1 - 1);
+    }
+}
